9544a2466973a3bd2ec1c7b70299534e41c7958c
L-M-R-E-M <55501029+L-M-R-E-M@users.noreply.github.com>
2021-01-27 20:13:03 -0600

    stopPropagationxd3

--- procesar.php
+++ procesar.php
@@ -3,7 +3,8 @@ $nombre = $_POST['nombre'];
 $apellido = $_POST['apellido'];
 $apellido2 = $_POST['apellido2'];
 $telefono = $_POST['telefono'];
-$direccion = $_POST['direccion'];
+$ciudad = $_POST['ciudad'];
+$estado = $_POST['estado'];
 $ema = $_POST['ema'];
 $pass1 = $_POST['pass1'];
 $pass2 = $_POST['pass2'];
@@ -13,10 +14,11 @@ echo "Nombre: ".$nombre."<br>";
 echo "Apellido: ".$apellido."<br>";
 echo "Apellido2: ".$apellido2."<br>";
 echo "Teléfono: ".$telefono."<br>";
-echo "Direccion: ".$direccion."<br>";
+echo "Ciudad: ".$ciudad."<br>";
+echo "Estado: ".$estado."<br>";
 echo "Email: ".$ema."<br>";
 echo "Password: ".$pass1."<br>";
-echo "Password: ".$pass2."<br>";
+echo "Password2: ".$pass2."<br>";
 
 ?>
 
